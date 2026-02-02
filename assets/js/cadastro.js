9714fdc407d7744a5920e7aea40a327ba7838499
rodrigocoder <rodriggotux@gmail.com>
2026-02-02 13:03:53 -0300

    feito a parte de cadastro no js

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,7 +1,43 @@
-console.log("Testing...");
+let createUser = document.querySelector(".btn-create");
 
-let formulario = document.querySelector(".btn-create");
+let password = document.querySelector("#password");
 
-formulario.addEventListener("click", () => {
-  alert("usuario cadastrado com sucesso");
+let confirm_password = document.querySelector("#confirm_password");
+
+const CreateUser = async () => {
+  try {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const dados = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({
+        user_name: "user_name",
+        nick: "nick",
+        email: "email",
+        password: "password",
+      }),
+    };
+    const response = await fetch(
+      "https://devbook-zqaw.onrender.com/usuarios/create",
+      dados,
+    );
+    console.log(response);
+  } catch (erro) {
+    console.log("Erro");
+  }
+};
+
+createUser.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  console.log("enviando formulario");
+
+  if (password.value != confirm_password.value) {
+    console.log("senha errada");
+  } else {
+    CreateUser();
+  }
 });
