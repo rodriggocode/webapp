7f7b2e9ec27d3e2bd24d20ae691cadc1cebbbaa1
rodrigocoder <rodriggotux@gmail.com>
2026-02-19 01:55:37 -0300

    fazendo a parte do cookie, estou com erro

---
+++ app/request/request.go
@@ -0,0 +1,25 @@
+package request
+
+import (
+	"io"
+	"net/http"
+	"webapp/app/cookies"
+)
+
+func RequestAuth(req *http.Request, method, url string, date io.Reader) (*http.Response, error) {
+	request, err := http.NewRequest(method, url, date)
+	if err != nil {
+		return nil, err
+	}
+
+	cookie, _ := cookies.ReadToken(req)
+	request.Header.Add("Authorization", "Bearer "+cookie["token"])
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
