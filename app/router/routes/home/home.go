26d30b0e778cddf8e2a2f90c65452b6db30062ae
rodrigocoder <rodriggotux@gmail.com>
2026-02-20 23:31:53 -0300

    mexendo em pages home, handlers home e rota home

--- app/router/routes/home/home.go
+++ app/router/routes/home/home.go
@@ -8,6 +8,6 @@ import (
 )
 
 func HomeRouter(router *http.ServeMux) {
-	router.HandleFunc("/home-handler", handlers.HomePage)                               // carrega minha logica com o back
+	router.HandleFunc("/home-posts", handlers.HomePage)                                 // carrega minha logica com o back
 	router.HandleFunc("/home", middlewares.Logger(middlewares.Auth(page.LoadHomePage))) // carrega minha pagina de home.html
 }
