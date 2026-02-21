c71d02959d0d82a70ec80d77461f589d50234242
rodrigocoder <rodriggotux@gmail.com>
2026-02-21 20:41:24 -0300

    alteracoes no js e handlers home

--- app/router/routes/home/home.go
+++ app/router/routes/home/home.go
@@ -3,11 +3,10 @@ package home
 import (
 	"net/http"
 	handlers "webapp/app/handlers/home"
-	"webapp/app/middlewares"
 	"webapp/app/pages"
 )
 
 func HomeRouter(router *http.ServeMux) {
-	router.HandleFunc("/posts", handlers.Home)                                           // vem da api
-	router.HandleFunc("/home", middlewares.Logger(middlewares.Auth(pages.LoadHomePage))) // html front
+	router.HandleFunc("/posts", handlers.Home)     // vem da api
+	router.HandleFunc("/home", pages.LoadHomePage) // html front
 }
