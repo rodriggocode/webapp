01d3789e6ef342eaea51858f43407af62ac4c0b6
rodriggomartins <rodriggodev@outlook.com>
2026-02-23 11:56:14 -0300

    coloquei um timeoute no console pra ver ser ele aparece

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,35 +3,35 @@ const loginEmail = document.querySelector("#email");
 
 const form = document.querySelector(".forms");
 const LoginForm = async () => {
-  const email = form.elements["email"].value;
-  const password = form.elements["password"].value;
-  try {
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    const date = {
-      method: "POST",
-      headers: headers,
-      credentials: "include",
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    };
-    const apiUrl = "https://devbook-zqaw.onrender.com/login";
-    const response = await fetch(apiUrl, date);
-    if (response.ok) {
-      console.log("Login bem-sucedido!");
-      window.location.assign("/home");
-    } else {
-      const errorData = await response.json();
+    const email = form.elements["email"].value;
+    const password = form.elements["password"].value;
+    try {
+        const headers = {
+            "Content-Type": "application/json",
+        };
+        const date = {
+            method: "POST",
+            headers: headers,
+            credentials: "include",
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        };
+        const apiUrl = "https://devbook-zqaw.onrender.com/login";
+        const response = await fetch(apiUrl, date);
+        if (response.ok) {
+            setTimeout(() => console.log("Login bem-sucedido!"), 2000);
+            window.location.assign("/home");
+        } else {
+            const errorData = await response.json();
+        }
+    } catch (erro) {
+        console.log("Erro na requisicao");
     }
-  } catch (erro) {
-    console.log("Erro na requisicao");
-  }
 };
 
 loginUser.addEventListener("click", (event) => {
-  event.preventDefault();
-  LoginForm();
+    event.preventDefault();
+    LoginForm();
 });
