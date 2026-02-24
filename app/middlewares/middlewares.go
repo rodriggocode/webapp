cbb8117b41c7ad64f4c49854404bdff96709b51a
rodriggomartins <rodriggodev@outlook.com>
2026-02-24 18:33:08 -0300

    tentando arrumar a home ainda

--- app/middlewares/middlewares.go
+++ app/middlewares/middlewares.go
@@ -3,7 +3,6 @@ package middlewares
 import (
 	"log"
 	"net/http"
-	"webapp/app/cookies"
 )
 
 // escreve infomacoes da requisicao no termianl
@@ -17,21 +16,22 @@ func Logger(nextFunction http.HandlerFunc) http.HandlerFunc {
 // verifica a existencia de cookies
 func Auth(nextFunction http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		token, err := cookies.ReadToken(req)
-		if err != nil {
-			http.Redirect(w, req, "/logar", 302)
-			return
-		}
+
+		// faz a requisição ao backend passando o cookie
 		apiReq, _ := http.NewRequest("GET", "https://devbook-zqaw.onrender.com/publicacoes", nil)
-		apiReq.Header.Set("Authorization", "Bearer "+token)
+
+		// repassa o cookie que o navegador enviou
+		apiReq.Header.Set("Cookie", req.Header.Get("Cookie"))
 		apiReq.Header.Set("Content-Type", "application/json")
 
 		client := &http.Client{}
 		resp, err := client.Do(apiReq)
+
 		if err != nil || resp.StatusCode != http.StatusOK {
-			http.Redirect(w, req, "/logar", 303)
+			http.Redirect(w, req, "/logar", 302)
 			return
 		}
+
 		nextFunction(w, req)
 	}
 }
