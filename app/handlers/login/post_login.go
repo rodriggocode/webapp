fa0086deb61a33160563306b708bb69aaefd2e2e
rodriggomartins <rodriggodev@outlook.com>
2026-02-24 19:55:19 -0300

    tentando arrumar a home ainda

--- app/handlers/login/post_login.go
+++ app/handlers/login/post_login.go
@@ -8,54 +8,59 @@ import (
 	"net/http"
 	"webapp/app/cookies"
 	"webapp/app/models"
+	"webapp/app/response"
 	resposta "webapp/app/response"
 )
 
 func PostLogin(w http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
+	var credentials struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
-	log.Printf("Email rebido: %s", req.FormValue("Email"))
-
-	user, err := json.Marshal(map[string]string{
-		"email":    req.FormValue("email"),
-		"password": req.FormValue("password"),
-	})
-	if err != nil {
+	if err := json.NewDecoder(req.Body).Decode(&credentials); err != nil {
 		resposta.JSON(w, http.StatusBadRequest, resposta.Err{Erro: err.Error()})
 		return
 	}
 
-	urlApi := "https://devbook-zqaw.onrender.com/login"
+	log.Printf("Email recebido: %s", credentials.Email)
 
-	response, err := http.Post(urlApi, "application/json", bytes.NewBuffer(user))
+	user, err := json.Marshal(map[string]string{
+		"email":    credentials.Email,
+		"password": credentials.Password,
+	})
+
 	if err != nil {
-		resposta.JSON(w, http.StatusInternalServerError, resposta.Err{Erro: err.Error()})
+		resposta.JSON(w, http.StatusBadRequest, resposta.Err{Erro: err.Error()})
+		return
+	}
+	urlApi := "https://devbook-zqaw.onrender.com/login"
+	resp, err := http.Post(urlApi, "application/json", bytes.NewBuffer(user))
+	if err != nil {
+		resposta.JSON(w, http.StatusInternalServerError, response.Err{Erro: err.Error()})
 		return
 	}
 
-	defer response.Body.Close()
+	defer resp.Body.Close()
 
-	if response.StatusCode >= 400 {
-		resposta.StatusCodeErr(w, response)
+	if resp.StatusCode >= 400 {
+		resposta.StatusCodeErr(w, resp)
 		return
 	}
 
 	var dateAuth models.DadoAtuh
-	if err = json.NewDecoder(response.Body).Decode(&dateAuth); err != nil {
+	if err = json.NewDecoder(resp.Body).Decode(&dateAuth); err != nil {
 		resposta.JSON(w, http.StatusUnprocessableEntity, resposta.Err{Erro: err.Error()})
 		return
 	}
 
-	if response.StatusCode >= 200 && response.StatusCode < 300 {
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
 		cookies.SetCookie(w, dateAuth.Token)
-
 		resposta.JSON(w, http.StatusOK, map[string]string{
 			"redirect": "/home",
 		})
-
 		return
 	}
 
 	resposta.JSON(w, http.StatusOK, nil)
-
 }
