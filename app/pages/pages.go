0cd58b10ffee267314090e8b41c6d70029de9d24
rodriggomartins <rodriggodev@outlook.com>
2026-02-26 16:51:40 -0300

    mexendo em pages de publicacoes na parte do home, e no home.html

--- app/pages/pages.go
+++ app/pages/pages.go
@@ -2,13 +2,10 @@
 package pages
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"webapp/app/models"
 	"webapp/app/request"
-	"webapp/app/response"
 	utils "webapp/app/utils"
 )
 
@@ -21,21 +18,10 @@ func LoadHomePage(w http.ResponseWriter, req *http.Request) {
 	resp, err := request.RequestAuth(req, http.MethodGet, url, nil)
 	if err != nil {
 		log.Printf("Erro ao buscar publicacoes: %v", err)
-		response.JSON(w, http.StatusInternalServerError, response.Err{Erro: err.Error()})
 		utils.ExecuterTemplate(w, "home.hml", nil)
 		return
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode >= 400 {
-		response.StatusCodeErr(w, resp)
-		return
-	}
-
-	var posts []models.Posts
-	if err = json.NewDecoder(req.Body).Decode(&posts); err != nil {
-		response.JSON(w, http.StatusUnprocessableEntity, response.Err{Erro: err.Error()})
-		return
-	}
 	fmt.Println(resp.StatusCode, err)
-	utils.ExecuterTemplate(w, "home.html", posts)
+	utils.ExecuterTemplate(w, "home.html", nil)
 }
