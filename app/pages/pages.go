8aadb73cca1bc7c5596f1c4f71932a6b1c5b3eae
rodriggomartins <rodriggodev@outlook.com>
2026-02-25 22:58:53 -0300

    mexendo na parte de requisicoes

--- app/pages/pages.go
+++ app/pages/pages.go
@@ -4,7 +4,10 @@ package pages
 import (
 	// Keep this import
 
+	"fmt"
 	"net/http"
+	"webapp/app/config"
+	"webapp/app/request"
 	utils "webapp/app/utils"
 )
 
@@ -13,5 +16,8 @@ func LoadPageCreateUser(w http.ResponseWriter, req *http.Request) {
 }
 
 func LoadHomePage(w http.ResponseWriter, req *http.Request) {
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	resp, err := request.RequestAuth(req, http.MethodGet, url, nil)
+	fmt.Println(resp.StatusCode, err)
 	utils.ExecuterTemplate(w, "home.html", nil)
 }
