3e9eacc7725b5d564cdf8da3c8efa8c115b6262a
rodriggomartins <rodriggodev@outlook.com>
2026-02-26 23:14:06 -0300

    mexendo em pages de publicacoes na parte do home, e no home.html

--- app/pages/pages.go
+++ app/pages/pages.go
@@ -17,7 +17,7 @@ func LoadPageCreateUser(w http.ResponseWriter, req *http.Request) {
 }
 
 func LoadHomePage(w http.ResponseWriter, req *http.Request) {
-	url := fmt.Sprintf("%s/publicacoes", "https://devbook-zqaw.onrender.com/publicacoes")
+	url := fmt.Sprintf("https://devbook-zqaw.onrender.com/publicacoes")
 	resp, err := request.RequestAuth(req, http.MethodGet, url, nil)
 	if err != nil {
 		log.Printf("Erro ao buscar publicacoes: %v", err)
