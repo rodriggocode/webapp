38f033c6c69026c262bcb23cdf9e51bbb7a3aa03
rodriggomartins <rodriggodev@outlook.com>
2026-03-03 11:57:05 -0300

    mexendo no home.html

--- app/models/posts.go
+++ app/models/posts.go
@@ -5,9 +5,10 @@ import "time"
 type Posts struct {
 	ID         uint64    `json:"id,omitempty"`
 	Title      string    `json:"title,omitempty"`
-	Contet     string    `json:"content,omitempty"`
+	Content    string    `json:"content,omitempty"`
 	AuthorID   uint64    `json:"author_id,omitempty"`
 	AuthorNick string    `json:"author_nick,omitempty"`
 	Like       uint64    `json:"like"`
 	CreatedAt  time.Time `json:"createdAt,omitempty"`
 }
+
