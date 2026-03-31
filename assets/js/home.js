cea1beff6e855bf1ff5f133c69064dbe8b9a59a0
rodrigocoder <rodriggotux@gmail.com>
2026-03-31 20:38:43 -0300

    alteracoes no front

--- assets/js/home.js
+++ assets/js/home.js
@@ -10,8 +10,14 @@ const Like = () => {
   let cont = document.querySelector("#count");
   cont.innerHTML = parseInt(cont.innerHTML) + 1;
 
+  let icon = document.getElementById("like-icon");
+  icon.style.fill = "red";
 }
+
+
 
 like.addEventListener("click", () => {
   Like();
 });
+
+
