8486e1f101898c5834dd9b92ba68392af429157e
rodrigocoder <rodriggotux@gmail.com>
2026-03-31 00:32:12 -0300

    alteracoes no front

---
+++ assets/js/home.js
@@ -0,0 +1,17 @@
+logout = document.querySelector(".logout");
+logout.addEventListener("click", () => {
+  window.location.href = "/";
+});
+
+
+let like = document.querySelector(".like");
+
+const Like = () => {
+  let cont = document.querySelector("#count");
+  cont.innerHTML = parseInt(cont.innerHTML) + 1;
+
+}
+
+like.addEventListener("click", () => {
+  Like();
+});
